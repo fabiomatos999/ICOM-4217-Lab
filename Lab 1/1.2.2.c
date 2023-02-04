7be4c95f4b22e03fcd98b36fed485b7f12b8f2d4
fabiomatos999 <78383665+fabiomatos999@users.noreply.github.com>
2023-02-04 16:36:58 -0400

    Update 1.2.2.c

--- Lab 1/1.2.2.c
+++ Lab 1/1.2.2.c
@@ -1,69 +1,50 @@
-#include <msp430.h>				
+#include <msp430.h>
 
 
-/**
- * blink.c
- */
 void init(){
-    for(i=0; i < 1250000; i++);
-    P3OUT |= 0x40; //
-    P3OUT &= 0xDE; //
-    P4OUT |= 0x60; // Function Set
-    P4OUT &= 0x7F; //
-    P5OUT &= 0xEF; //
-    for(i=0; i < 125000; i++);
-    P3OUT |= 0x40; //
-    P3OUT &= 0xDE; //
-    P4OUT |= 0x60; // Function Set
-    P4OUT &= 0x7F; //
-	
-    P5OUT &= 0xEF; //
-    for(i=0; i < 3750; i++);
-    P3OUT &= 0xDE; //
-    P3OUT |= 0x80; //
-    P5OUT &= 0xEF; // Function Set with 1 line and 5x8 character font
-    P4OUT &= 0x80; //
-    P4OUT |= 0x60; //
+    volatile unsigned int j;
+    for(j=0; j < 50000; j++);
+    P3OUT ^= 0x10;
+    P4OUT = 0x60;
+    P3OUT ^= 0x10;
+    for(j=0; j < 4500; j++);
+    P3OUT ^= 0x10;
+    P3OUT ^= 0x10;
+    for(j=0; j < 450; j++);
 
-    P3OUT &= 0xDE; //
-    P3OUT |= 0x80; //
-    P5OUT &= 0xEF; // Display Off
-    P4OUT &= 0x18; //
-    P4OUT |= 0x08; //
+    P3OUT ^= 0x10;
+    P4OUT = 0x68;
+    P3OUT ^= 0x10;
 
-    P3OUT &= 0xDE; //
-    P3OUT |= 0x80; //
-    P5OUT &= 0xEF; // Clear Display
-    P4OUT &= 0xEE; //
-    P4OUT |= 0x01; //
+    P3OUT ^= 0x10;
+    P4OUT = 0x08;
+    P3OUT ^= 0x10;
 
-    P3OUT &= 0xDE; //
-    P3OUT |= 0x80; //
-    P5OUT &= 0xEF; // Entry mode set
-    P3OUT &= 0x16; //
-    P3OUT |= 0x04; //
-    P3OUT |= 0x02; //
+    P3OUT ^= 0x10;
+    P4OUT = 0x01;
+    P3OUT ^= 0x10;
 
-    P3OUT &= 0xDE; //
-    P3OUT |= 0x80; //
-    P5OUT &= 0xEF; //
-    P3OUT &= 0x1F; // Display On/Off
-    P3OUT |= 0x08; //
-    P3OUT &= 0xF7; //
+    P3OUT ^= 0x10;
+    P4OUT = 0x06;
+    P3OUT ^= 0x10;
 
+    P3OUT ^= 0x10;
+    P4OUT = 0x0D;
+    P3OUT ^= 0x10;
 
 }
 void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P3DIR = 0x31;  // P3.0 = R/W, P3.5 = RS, P3.6 = E
-	P4DIR= 0xEF; // D0 = P4.0,D1 = P4.1, D2 = P4.2, D3 = P4.3, D4 = P4.5, D5 = P4.6, D6 = P4.7
-	P5DIR = 0x10; // D7 = P5.4
+	P3DIR = 0xF0;
+	P4DIR= 0xEF;
+	P3OUT = 0x00;
 	init();
 
 
 	volatile unsigned int i;		// volatile to prevent optimization
 
-	while true {
+	while (1) {
+
 	}
 }
