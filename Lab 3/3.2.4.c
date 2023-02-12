648259003b5e7d0b73a50941bdeff8019d6c2d52
fabiomatos999 <78383665+fabiomatos999@users.noreply.github.com>
2023-02-12 12:54:53 -0400

    Can detect columns.

--- Lab 3/3.2.4.c
+++ Lab 3/3.2.4.c
@@ -2,13 +2,13 @@
 void main()
 {
 
-    P1DIR &= 0x70; // P1.4 = Column 1, P1.5 = Column 2, P1.6 = Column 3
+    P1DIR &= ~0x70; // P1.4 = Column 1, P1.5 = Column 2, P1.6 = Column 3
     P1REN |= 0x70;
     P1IFG &= ~0x70;
     P1IES |= 0x70;
     P1IE |= 0x70;
-    P4DIR |= 0x00; // P4.0 = Row 1, P4.1 = Row 2, P4.2 = Row 3, P4.3 = Row 4
-    P4OUT |= 0xFF;
+    P4DIR |= 0x0F; // P4.0 = Row 1, P4.1 = Row 2, P4.2 = Row 3, P4.3 = Row 4
+    P4OUT |= 0x0F;
     __enable_interrupt();
     while (1){
     }
@@ -17,8 +17,8 @@ void main()
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT_1()
 {
-    int column1 = (P1IN >> 4) & 1;
-    int column2 = (P1IN >> 5) & 1;
-    int column3 = (P1IN >> 6) & 1;
-
+    unsigned volatile int column1 = (P1IFG >> 4) & 1;
+    unsigned volatile int column2 = (P1IFG >> 5) & 1;
+    unsigned volatile int column3 = (P1IFG >> 6) & 1;
+    P1IFG &= ~0x70;
 }
