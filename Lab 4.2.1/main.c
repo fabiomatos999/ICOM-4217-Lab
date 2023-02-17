a725f693f9e21324b4b643acfa05be358f2002a2
fabiomatos999 <78383665+fabiomatos999@users.noreply.github.com>
2023-02-17 14:34:43 -0400

    Add files via upload

--- Lab 4.2.1/main.c
+++ Lab 4.2.1/main.c
@@ -4,18 +4,31 @@
 /**
  * main.c
  */
+// Bit Fields: https://www.tutorialspoint.com/cprogramming/c_bit_fields.htm
+long half_periods[5] = {0x40,0x20,0x10,0x08,0x04};
+int index = 0;
+int half_period = half_periods[index];
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	TA0CTL = 0x8A;
-	TA0CCR0 = 0x4000;
-	P1DIR = 0x04;
-	P1OUT = 0x04;
+	TA0CTL |= 0x112;
+	TA0CCR0 |= 0x20;
+	P1DIR |= 0x04;
+	P1OUT |= 0x04;
+	P1REN |= 0x10;
+	P1IFG &= ~0x10;
+	P1IES |= 0x10;
+	P1IE |= 0x10;
     __enable_interrupt();
 	
 	while (1) {
-	    if(TA0R >= 0x4000){
+	    if(TA0R >= 0x20){
+	        TA0R = 0;
 	        P1OUT ^= 0x04;
 	    }
 	}
+}
+#pragma vector=PORT1_VECTOR
+__interrupt void PORT_1(){
+
 }
