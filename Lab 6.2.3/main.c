bfd9e81b83f26b96a8c2d4bdb84ac6a2a00b59be
fabiomatos999 <78383665+fabiomatos999@users.noreply.github.com>
2023-04-02 12:39:49 -0400

    Fixed buggs in 6.2.3

--- Lab 6.2.3/main.c
+++ Lab 6.2.3/main.c
@@ -5,7 +5,7 @@
  * main.c
  */
 
-float angles[] = {-67.5,22.5,0,45,-45,90,67.5,-22.5,0};
+float angles[] = {-67.5,22.5,0,45,-45,90,67.5,-22.5,-90};
 unsigned int angle = 0;
 
 void send_to_degree(float deg);
@@ -22,24 +22,24 @@ int main(void)
     P1SEL |= BIT2;
     TA0CCTL1 |= OUTMOD_7;
 
-    send_to_degree(angles[angle]);
-
-    TB0CTL |= 0x110;
-    TB0CCTL0 |= 0x10;
-    TB0CCR0 = 0x8000;
+    TA1CTL |= 0x110;
+    TA1CCTL0 |= 0x10;
+    TA1CCR0 = 0xFF00;
     __enable_interrupt();
-    _bis_SR_register(LPM0_bits);
+    send_to_degree(0);
+    while(1){
+
+    }
 }
 
 void send_to_degree(float deg){
     if(deg < -90 && deg > 90){return;}
-    volatile float dc = ((deg/180.0)+(3.0/2.0))/20;
+    volatile float dc = ((deg/90.0)+(1.5))/20.0;
     TA0CCR1 = dc*TA0CCR0;
 }
 
-#pragma vector=TIMER0_B0_VECTOR
-__interrupt void Timer_B(void){
-    angle = (++angle)%9;
-    unsigned volatile int uwu = angle;
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer_A1(void){
     send_to_degree(angles[angle]);
+    angle = (++angle)%9;
 }
