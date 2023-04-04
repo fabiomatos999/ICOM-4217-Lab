4f15eb4edd75d1115f93086933241d752a8fc062
fabiomatos999 <78383665+fabiomatos999@users.noreply.github.com>
2023-04-04 09:54:28 -0400

    Finished 6.2.4

--- Lab 6.2.4/main.c
+++ Lab 6.2.4/main.c
@@ -1,83 +1,107 @@
 #include <msp430.h> 
 
-typedef enum direction {CW,CCW,NONE} direction;
+typedef enum direction
+{
+    CW, CCW, NONE
+} direction;
 
 void moveFullStep(direction dir);
 void moveHalfStep(direction dir);
 void moveDeg(unsigned int deg, direction dir);
 
-typedef struct input {
-    unsigned int a:1;
-    unsigned int b:1;
-    unsigned int abar:1;
-    unsigned int bbar:1;
+
+typedef struct input
+{
+    unsigned int a :1;
+    unsigned int b :1;
+    unsigned int abar :1;
+    unsigned int bbar :1;
 } input;
 
+input fullstep[4] = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0,
+                                                                        1 } };
+input halfstep[8] = { { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 1,
+                                                                        0 },
+                      { 0, 0, 1, 0 }, { 0, 0, 1, 1 }, { 0, 0, 0, 1 }, { 1, 0, 0,
+                                                                        1 } };
 
-
-input fullstep[4] = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
-input halfstep[8] = {{1,0,0,0},{1,1,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0},{0,0,1,1},{0,0,0,1},{1,0,0,1}};
-
-unsigned int currstep = 0;
+int currstep = 0;
+unsigned int degree = 0;
 direction currdirr = NONE;
+unsigned int stepcount = 0;
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
     TA0CTL = TASSEL_1 + MC_1;
     TA0CCTL0 = CCIE;
-    TA0CCR0 = 32768/100;
-    P1DIR = 0x0F;
-	__enable_interrupt();
-    _bis_SR_register(LPM0_bits);
+    TA0CCR0 = 32768 / 100;
+    P10DIR = 0x0F;
+
+    __enable_interrupt();
+    moveDeg(270, CCW);
+    moveDeg(180, CCW);
+    moveDeg(180, CW);
+    while(1){}
 }
 
-void moveFullStep(direction dir){
-    if (dir == CW) {
-        currstep = (++currstep)%4;
-        currdirr = CW;
+void moveFullStep(direction dir)
+{
+    volatile int step;
+    P10OUT = ((fullstep[currstep].a << 0) + (fullstep[currstep].b << 1)
+            + (fullstep[currstep].abar << 2) + (fullstep[currstep].bbar << 3));
+    if (dir == CW)
+    {
+        currstep = (++currstep) % 4;
+    }
+    else if (dir == CCW)
+    {
+        currstep -= 1;
+        if (currstep < 0){
+            currstep = 3;
+        }
+    }
+    return;
 
-    }
-    else if (dir == CCW) {
-        currstep = (--currstep)%4;
-        currdirr = CCW;
-    }
-    else {
-        currdirr = NONE;
-        return;
-    }
-    volatile input step = fullstep[currstep];
-    P1OUT = ~((fullstep[currstep].a<<0) + (fullstep[currstep].b<<1) + (fullstep[currstep].abar<<2) + (fullstep[currstep].abar<<3));
 }
 
-void moveHalfStep(direction dir){
-    if (dir == CW) {
-        currstep = (++currstep)%8;
-        currdirr = CW;
+void moveHalfStep(direction dir)
+{
+    P10OUT = ((halfstep[currstep].a << 0) + (halfstep[currstep].b << 1)
+            + (halfstep[currstep].abar << 2) + (halfstep[currstep].bbar << 3));
+    if (dir == CW)
+    {
+        currstep = (++currstep) % 8;
+    }
+    else if (dir == CCW)
+    {
+        currstep -= 1;
+        if (currstep < 0){
+            currstep = 7;
+        }
+    }
+    return;
 
-    }
-    else if (dir == CCW) {
-        currstep = (--currstep)%8;
-        currdirr = CCW;
-
-    }
-    else {
-        currdirr = NONE;
-        return;
-    }
-    P1OUT = ~((halfstep[currstep].a<<0) + (halfstep[currstep].b<<1) + (halfstep[currstep].abar<<2) + (halfstep[currstep].bbar<<3));
 }
 
 #pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A(void){
-    if (currdirr != NONE) {
+__interrupt void Timer_A(void)
+{
+    if(stepcount > degree/5.625)
+    {
+        stepcount = 0;
+        currdirr = NONE;
+    }
+    if (currdirr != NONE)
+    {
+        stepcount += 1;
         moveFullStep(currdirr);
     }
 }
 
-void moveSteps(unsigned int deg, direction dir){
-//    if(dir == CW){
-//        unsigned int i = 0;
-//        for(i;i<deg,15,)
-//    }
+void moveDeg(unsigned int deg, direction dir)
+{
+    currdirr = dir;
+    degree = deg;
+    while (currdirr != NONE){}
 }
