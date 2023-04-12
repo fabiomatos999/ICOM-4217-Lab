4a6df1c101cc705c41aa8432725f1c0cd09a7bed
GuillermoColonBernardi <70712369+GuillermoColonBernardi@users.noreply.github.com>
2023-04-12 15:09:18 -0400

    Comments added
    Comments added to LAB 6

--- Lab 6.2.3/main.c
+++ Lab 6.2.3/main.c
@@ -5,8 +5,8 @@
  * main.c
  */
 
-float angles[] = {-67.5,22.5,0,45,-45,90,67.5,-22.5,-90};
-unsigned int angle = 0;
+float angles[] = {-67.5,22.5,0,45,-45,90,67.5,-22.5,-90}; // Declare and initialize an array of angles
+unsigned int angle = 0; // Declare and initialize a variable for the current angle
 
 void send_to_degree(float deg);
 
@@ -14,32 +14,33 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
     //Timer A0 Setup
-    TA0CTL |= TASSEL_2 + MC_1;
-    TA0CCR0 = 1048576.0/50.0; // 50 Hz
+    TA0CTL |= TASSEL_2 + MC_1; // Select SMCLK as the clock source and put the timer into Up mode
+    TA0CCR0 = 1048576.0/50.0;  // Set the value in TA0CCR0 to achieve a frequency of 50Hz
 
     //Timer A0 CC1 Setup (RED)
-    P1DIR |= BIT2;
-    P1SEL |= BIT2;
-    TA0CCTL1 |= OUTMOD_7;
+    P1DIR |= BIT2; // Set P1.2 as an output
+    P1SEL |= BIT2; // Select Timer A0 CC1 output for P1.2
+    TA0CCTL1 |= OUTMOD_7; // Set the "Reset/Set" output mode for Timer A0 CC1
 
-    TA1CTL |= 0x110;
-    TA1CCTL0 |= 0x10;
-    TA1CCR0 = 0xFF00;
-    __enable_interrupt();
-    send_to_degree(0);
+    TA1CTL |= 0x110; // Set the clock source to SMCLK, put the timer into Up mode, and enable the interrupt
+    TA1CCTL0 |= 0x10; // Enable interrupt for Timer A1 Capture/Compare 0
+    TA1CCR0 = 0xFF00; // Set the value in TA1CCR0 to trigger the interrupt every 65536 cycles of the SMCLK
+    __enable_interrupt(); // Enable interrupts globally
+    send_to_degree(0); // Set the servo motor to the neutral position
     while(1){
 
     }
 }
 
+// Function to set the angle of the servo motor
 void send_to_degree(float deg){
-    if(deg < -90 && deg > 90){return;}
-    volatile float dc = ((deg/90.0)+(1.5))/20.0;
-    TA0CCR1 = dc*TA0CCR0;
+    if(deg < -90 && deg > 90){return;} // Check if the angle is within the valid range
+    volatile float dc = ((deg/90.0)+(1.5))/20.0; // Calculate the duty cycle
+    TA0CCR1 = dc*TA0CCR0; // Set the duty cycle for Timer A0 CC1
 }
 
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void Timer_A1(void){
-    send_to_degree(angles[angle]);
-    angle = (++angle)%9;
+    send_to_degree(angles[angle]); // Set the angle of the servo motor
+    angle = (++angle)%9; // Increment the angle index and wrap around if necessary
 }
--- Lab 6.3.1/main.c
+++ Lab 6.3.1/main.c
@@ -17,12 +17,15 @@ typedef struct input
     unsigned int bbar :1;
 } input;
 
+// Define two arrays of "input" type that hold different step sequences for a stepper motor
 input fullstep[4] = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0,
                                                                         1 } };
 input halfstep[8] = { { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 1,
                                                                         0 },
                       { 0, 0, 1, 0 }, { 0, 0, 1, 1 }, { 0, 0, 0, 1 }, { 1, 0, 0,
                                                                         1 } };
+
+// Defines an array of unsigned long integers that holds different delay periods for the stepper motor
 unsigned long periods[] = {104857, 52428, 20971, 10485, 5242, 2097, 1048, 524, 209, 104 };
 int period = 0;
 
@@ -34,19 +37,19 @@ unsigned long stepcount = 0;
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    TA0CTL = TASSEL_2 + MC_1;
-    TA0CCTL0 = CCIE;
-    TA0CCR0 = periods[period];
-    P10DIR = 0x0F;
+    TA0CTL = TASSEL_2 + MC_1;   // Set Timer_A clock source to SMCLK, and count up mode 
+    TA0CCTL0 = CCIE; // Enable Timer_A capture/compare interrupt
+    TA0CCR0 = periods[period]; // Set the compare value for Timer_A to the value at index 0 of "periods" array
+    P10DIR = 0x0F; // Set Port 10 pins 0-3 as output
 
-    P1REN |= BIT4 + BIT2 + BIT5;
-    P1IES |= (BIT2 + BIT4 + BIT5);
-    P1IFG &= ~(BIT2 + BIT4 + BIT5);
-    P1IE |= BIT2 + BIT4 + BIT5;
-    P10DIR |= BIT0 + BIT1;
+    P1REN |= BIT4 + BIT2 + BIT5; // Enable pull-up resistor for Port 1 pins 2, 4 and 5
+    P1IES |= (BIT2 + BIT4 + BIT5); // Set interrupt edge select to trigger on falling edge
+    P1IFG &= ~(BIT2 + BIT4 + BIT5); // Clear Port 1 interrupt flags for pins 2, 4, and 5
+    P1IE |= BIT2 + BIT4 + BIT5; // Enable interrupt for Port 1 pins 2, 4, and 5
+    P10DIR |= BIT0 + BIT1; // Set direction of Port 10 pins 0 and 1 as output
 
     __enable_interrupt();
-    currdirr = CW;
+    currdirr = CW; // Set current direction as clockwise
     while (1)
     {
     }
@@ -55,8 +58,12 @@ int main(void)
 void moveFullStep(direction dir)
 {
     volatile int step;
-    P10OUT = ((fullstep[currstep].a << 0) + (fullstep[currstep].b << 1)
+
+    // sets the output of Port 10 pins 0 and 1 based on the current step in the sequence + sets the output of Port 10 pins 2 and 3 based on the current step in the sequence
+    P10OUT = ((fullstep[currstep].a << 0) + (fullstep[currstep].b << 1) 
             + (fullstep[currstep].abar << 2) + (fullstep[currstep].bbar << 3));
+
+
     if (dir == CCW)
     {
         currstep = (++currstep) % 3;
@@ -73,32 +80,33 @@ void moveFullStep(direction dir)
 
 }
 
-void moveHalfStep(direction dir)
+void moveHalfStep(direction dir) // A function that takes in the direction parameter and moves the motor by half a step in the specified direction
 {
     P10OUT = ((halfstep[currstep].a << 0) + (halfstep[currstep].b << 1)
-            + (halfstep[currstep].abar << 2) + (halfstep[currstep].bbar << 3));
+            + (halfstep[currstep].abar << 2) + (halfstep[currstep].bbar << 3)); // Set the motor pins according to the current step
     if (dir == CCW)
     {
-        currstep = (++currstep) % 3;
+        currstep = (++currstep) % 3; // Increment the current step by 1 and set it back to 0 if it exceeds 2
     }
     else if (dir == CW)
     {
         currstep -= 1;
-        if (currstep < 0)
+        if (currstep < 0) // If the current step is less than 0 (underflow)
         {
-            currstep = 3;
+            currstep = 3; // Set the current step to the maximum step value (3)
         }
     }
     return;
 
 }
 
-#pragma vector=TIMER0_A0_VECTOR
+#pragma vector=TIMER0_A0_VECTOR // specifies that the following function is an interrupt service routine (ISR) for Timer_A
 __interrupt void Timer_A(void)
 {
-    volatile int a = degree/(5.625/8);
-    volatile int b = stepcount;
-    if(stepcount > degree/(5.625/8))
+    volatile int a = degree/(5.625/8); // calculates the number of steps to rotate the stepper motor by dividing the desired degree value by the angle per step and saves it in a volatile integer variable 'a'
+    volatile int b = stepcount; 
+    if(stepcount > degree/(5.625/8)) 
+
     {
         stepcount = 0;
         currdirr = NONE;
@@ -116,19 +124,19 @@ void moveDeg(unsigned int deg, direction dir)
     degree = deg;
 }
 
-#pragma vector=PORT1_VECTOR
-__interrupt void PORT_1()
+#pragma vector=PORT1_VECTOR // specifies that the following function is an ISR for Port 1
+__interrupt void PORT_1() 
 {
-    unsigned int b1 = (P1IFG >> 2) & 1;
-    unsigned int b2 = (P1IFG >> 4) & 1;
-    unsigned int b3 = (P1IFG >> 5) & 1;
+    unsigned int b1 = (P1IFG >> 2) & 1; // checks if the interrupt flag for Pin 2 of Port 1 is set and stores the result in an unsigned integer variable 'b1'
+    unsigned int b2 = (P1IFG >> 4) & 1; // checks if the interrupt flag for Pin 4 of Port 1 is set and stores the result in an unsigned integer variable 'b2'
+    unsigned int b3 = (P1IFG >> 5) & 1; // checks if the interrupt flag for Pin 5 of Port 1 is set and stores the result in an unsigned integer variable 'b3'
     volatile int index;
     if (b1)
     {
         if (currdirr == NONE)
         {
             currdirr = CW;
-            moveDeg(720,CW);
+            moveDeg(720,CW); // calls the 'moveDeg' function with 720 degrees and 'CW' direction as arguments
         }
         else {
             currdirr = NONE;
